b99f685e1594d023fd034d2db747e522af9048e4
Nitesh Kalyanshetti <nvk8028@gmail.com>
2025-10-16 19:29:29 +0530

    Updated Conversation

--- app/src/main/java/com/example/ctchat/models/Conversation.kt
+++ app/src/main/java/com/example/ctchat/models/Conversation.kt
@@ -1,6 +1,11 @@
 package com.example.ctchat.models
 
 sealed class Conversation {
-    data class UserConversation(val user: User) : Conversation()
-    data class GroupConversation(val group: Group) : Conversation()
+    abstract val lastMessageTimestamp: Long
+    data class ChatSessionConversation(val session: ChatSession, val otherUser: User?) : Conversation() {
+        override val lastMessageTimestamp: Long get() = session.lastMessageTimestamp
+    }
+    data class GroupConversation(val group: Group) : Conversation() {
+        override val lastMessageTimestamp: Long get() = group.lastMessageTimestamp
+    }
 }
